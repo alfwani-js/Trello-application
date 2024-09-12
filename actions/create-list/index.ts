145c69ea20b825f91cf2719ff74cd8eef8340f2b
ian-Alfwani <146720713+ian-Alfwani@users.noreply.github.com>
2024-09-12 14:39:05 +0300

    list server3

--- actions/create-list/index.ts
+++ actions/create-list/index.ts
@@ -8,7 +8,7 @@ import { createSafeAction } from "@/lib/create-safe-action";
 const handler = async (data: InputType): Promise<ReturnType> =>{
     const {userId, orgId} = auth();
     if (!userId || !orgId){
-        return {
+        return { 
             error: "Unauthorized",
         };
     };
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,8 +1,12 @@
 "use client"
+import { toast } from "sonner";
 import { useState, useRef, ElementRef } from "react";
 import { List } from "@prisma/client";
 import { useEventListener } from "usehooks-ts";
 import { FormInput } from "@/components/form/form-input";
+import { updateList } from "@/actions/update-list";
+import { useAction } from "@/hooks/use-action";
+
 
 interface ListHeaderProps {
     data: List;
@@ -25,6 +29,37 @@ export const ListHeader = ({
     const disableEditing =()=>{
         setIsediting(false);
     }
+
+    const { execute } = useAction(updateList, {
+        onSuccess: (data) => {
+            toast.success(`Renamed to "${data.title}"`);
+            setTitle(data.title);
+            disableEditing();
+        },
+        onError: (error) => {
+            toast.error(error);
+        },
+    });
+    const handleSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+        const id = formData.get("title") as string;
+        const boardId = formData.get("title") as string;
+
+        if (title === data.title){
+            return disableEditing();
+        }
+
+        execute({
+            title,
+            id,
+            boardId,
+        });
+
+
+    }
+    const onBlur = () => {
+        formRef.current?.requestSubmit();
+    }
     const onKeyDown = (e: KeyboardEvent)=>{
         if (e.key === "Escape"){
             formRef.current?.requestSubmit();
@@ -37,17 +72,22 @@ export const ListHeader = ({
         <div 
         className="pt-2 px-2 text-sm font-semibold flex justify-between items-start- gap-x-2">
             {isEditing ?(
-                <form className="flex-1 px-[2px]">
+                <form 
+                    ref={formRef}
+                    action={handleSubmit}
+                    className="flex-1 px-[2px]">
                     <input  hidden id="id" name="name" value={data.id}/>
                     <input  hidden id="boardId" name="boardId" value={data.boardId}/>
                     <FormInput
                       ref={inputRef}
-                      onBlur={()=>{}}
+                      onBlur={onBlur}
                       id="title"
                       placeholder="Enter list title..."
                       defaultValue={title} 
+                      className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white "
                       
                     />
+                    <button type="submit" hidden/>
 
                 </form>
             ) : (
