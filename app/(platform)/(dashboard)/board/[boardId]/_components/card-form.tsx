6bd89517e71124f444dc442119e593d515638435
ian-Alfwani <146720713+ian-Alfwani@users.noreply.github.com>
2024-09-17 18:05:09 +0300

    card update1

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,6 +1,10 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
+import { FormTextarea } from "@/components/form/form-textarea";
+import { FormSubmit } from "@/components/form/form-submit";
 
 interface CardFormProps {
     listId: string;
@@ -16,9 +20,49 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
     isEditing
 
 }, ref) => {
+
+    if(isEditing) {
+        return(
+            <form 
+              className="m-1 py-0.5 px-1 space-y-4"
+            >
+                <FormTextarea
+                  id="title"
+                  onKeyDown={()=>{}}
+                  ref={ref}
+                  placeholder="Enter a title for this card..."
+                  
+                />
+                <input
+                  hidden
+                  id="listId"
+                  name="listId"
+                  value={listId}
+                />
+                <div className="flex items-center gap-x-1">
+                    <FormSubmit>
+                        Add Card
+                        <Button onClick={disableEditing} size="sm" variant="ghost">
+                            <X className="h-5 w-5"/>
+                        </Button>
+                    </FormSubmit>
+                </div>
+
+            </form>
+        )
+    }
     return (
-        <div>
-            Card Form
+        <div className="pt-2 px-2">
+            <Button
+              onClick={enableEditing}
+              className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+              size="sm"
+              variant="ghost"
+            >
+                <Plus className="h-4 w-4 mr-2"/>
+                Add a card
+            </Button>
+            
         </div>
     )
 });
